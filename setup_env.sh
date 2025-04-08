90199d0494fb126eb8de957b4e3209c3c781509a
Daniel Damiani <ddamiani@slac.stanford.edu>
2025-04-08 14:47:13 -0700

    update path to bfin-uclinux install

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,15 @@
-if [ -d /opt/uClinux/bfin-uclinux/bin ]; then
-  export PATH="/opt/uClinux/bfin-uclinux/bin:$PATH"
+if [ -d /cds/group/pcds/package/external/blackfin/2014R1 ]; then
+    BFIN_DEF_DIR=/cds/group/pcds/package/external/blackfin/2014R1
 else
-  export CROSS=""
+    BFIN_DEF_DIR=""
+fi
+
+BFIN_DIR="${BFIN_DIR-$BFIN_DEF_DIR}"
+
+if [ -d "${BFIN_DIR}/opt/uClinux/bfin-uclinux/bin" ]; then
+    echo "Using bfin install under ${BFIN_DIR}"
+    export PATH="${BFIN_DIR}/opt/uClinux/bfin-uclinux/bin:$PATH"
+    unset CROSS
+else
+    export CROSS=""
 fi
